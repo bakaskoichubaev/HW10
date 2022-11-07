646baf3fad2f0e0678148994623582f1c38200d5
99670 <bakaskoichubaev312@gmail.com>
2022-11-07 14:48:45 +0600

    9ioio

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+//        Triungle triungle = new Triunglee();
+//        triungle.setA(3);
+//        triungle.setB(3);
+//        triungle.setC(3);
+//
+//        System.out.println(triungle.getA());
+//        System.out.println(triungle.getB());
+//        System.out.println(triungle.getC());
+//        triungle.area();
+
+    }
+}
---
+++ src/Triungle.java
@@ -0,0 +1,8 @@
+public class Triungle extends Figura {
+    public Triungle(int a, int b, int c) {
+        super(a, b, c);
+    }
+}
+
+
+
